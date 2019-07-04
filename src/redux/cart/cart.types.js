cfc7c924ba99027181157a1ffdcab7557a17ddde
Fachrur Ridwan <ridwan.fachrur@gmail.com>
2019-07-04 22:54:20 +0700

    fix cart action and reducer for cart item to be implemented in cart dropdown

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,3 +1,11 @@
 export const CartActionTypes = {
-  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN'
+  TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+  ADD_ITEM: 'ADD_ITEM'
 }
+
+// const CartActionTypes = {
+//   TOGGLE_CART_HIDDEN: 'TOGGLE_CART_HIDDEN',
+//   ADD_ITEM: 'ADD_ITEM'
+// };
+
+// export default CartActionTypes;
