26ea37cec806a0487381fb4805002f07a9338ba9
Fachrur Ridwan <ridwan.fachrur@gmail.com>
2019-07-04 10:43:23 +0700

    set up routing, shop page, and form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
+import AccountPage from '../src/pages/account/AccountPage.component';
 import HomePage from '../src/pages/homepage/HomePage.component';
+import ShopPage from '../src/pages/shop/ShopPage.component';
+import Header from '../src/components/header/Header.component';
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Header />
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/shop' component={ShopPage}/>
+        <Route path='/account' component={AccountPage}/>
+      </Switch>
     </div>
   );
 }
---
+++ src/pages/account/AccountPage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/SignIn.component';
+
+import '../account/accountpage.styles.scss';
+
+const AccountPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn/>
+  </div>
+);
+
+export default AccountPage;
