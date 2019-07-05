d6d6f1a260d20344fe91713a952bce3d4b66a3c3
Fachrur Ridwan <ridwan.fachrur@gmail.com>
2019-07-05 10:01:49 +0700

    maintained localStorage using persist, created dir and collection states for collection overview component

--- src/pages/shop/ShopPage.component.jsx
+++ src/pages/shop/ShopPage.component.jsx
@@ -1,28 +1,11 @@
 import React from 'react';
 
-import SHOP_DATA from '../shop/Shop.data';
+import CollectionsOverview from '../../components/collections-overview/CollectionsOverview.component';
 
-import CollectionPreview from '../../components/collection-preview/CollectionPreview.component';
-
-class ShopPage extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
-
-  render() {
-    const {collections} = this.state;
-    return (<div className='shop-page'>
-      {
-        collections.map(({ id, ...otherCollectionProps }) => (
-          <CollectionPreview key={id} {...otherCollectionProps}/>
-        ))
-      }
-    </div>);
-  }
-}
+const ShopPage = ({ collections }) => (
+  <div className='shop-page'>
+    <CollectionsOverview/>
+  </div>
+);
 
 export default ShopPage;
