fc6a4aa68f42323a77917e02e0af18a6e3ae33fd
Fachrur Ridwan <ridwan.fachrur@gmail.com>
2019-07-08 16:18:31 +0700

    implemented CSS in JS using styled-components for CustomButton and Header

--- src/pages/homepage/HomePage.component.jsx
+++ src/pages/homepage/HomePage.component.jsx
@@ -2,12 +2,12 @@ import React from 'react';
 
 import Directory from '../../components/directory/Directory.component';
 
-import './homepage.styles.scss';
+import { HomePageContainer } from '../homepage/HomePage.styles';
 
 const HomePage = () => (
-  <div className='homepage'>
+  <HomePageContainer>
     <Directory />
-  </div>
+  </HomePageContainer>
 );
 
 export default HomePage;
