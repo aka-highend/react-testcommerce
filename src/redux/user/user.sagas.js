f23706e8e8d97bd51b7dc8761270fe56b8207232
Fachrur Ridwan <ridwan.fachrur@gmail.com>
2019-07-30 00:57:06 +0700

    concised user signInSuccess and signInFailure

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -2,7 +2,7 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import UserActionTypes from '../user/user.types';
 
-import { googleSignInSuccess, googleSignInFailure, emailSignInSuccess, emailSignInFailure } from '../user/user.action';
+import { signInSuccess, signInFailure } from '../user/user.action';
 
 import { auth, googleProvider, createUserProfileDocument } from '../../firebase/firebase.utils';
 
@@ -12,12 +12,12 @@ export function* signInWithGoogle() {
     const userRef = yield call(createUserProfileDocument, user);
     const userSnapshot = yield userRef.get();
     yield put(
-      googleSignInSuccess({ 
+      signInSuccess({ 
         id: userSnapshot.id,
         ...userSnapshot.data()
     }));
   } catch (error) {
-    yield put(googleSignInFailure(error));
+    yield put(signInFailure(error));
   }
 }
 
@@ -27,12 +27,12 @@ export function* signInWithEmail({ payload: { email, password } }) {
     const userRef = yield call(createUserProfileDocument, user);
     const userSnapshot = yield userRef.get();
     yield put(
-      emailSignInSuccess({ 
+      signInSuccess({ 
         id: userSnapshot.id,
         ...userSnapshot.data()
     }));
   } catch (error) {
-    yield put(emailSignInFailure(error));
+    yield put(signInFailure(error));
   }
 }
 
